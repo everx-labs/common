6854cbb3df70046ddc46240755ee24b0df44e79e
sergeyyar <sergey.yar.1979@gamil.com>
2022-06-30 14:46:24 +0300

    add rust version

--- build/build.rs
+++ build/build.rs
@@ -26,10 +26,11 @@ fn main() {
     let git_branch = get_value("git", &["rev-parse", "--abbrev-ref", "HEAD"]);
     let git_commit = get_value("git", &["rev-parse", "HEAD"]);
     let commit_date = get_value("git", &["log", "-1", "--date=iso", "--pretty=format:%cd"]);
-    let build_time = get_value("date", &["+%Y-%m-%d %T %z"]);
+    let rust_version = get_value("rustc", &["--version"]);
 
     println!("cargo:rustc-env=BUILD_GIT_BRANCH={}", git_branch);
     println!("cargo:rustc-env=BUILD_GIT_COMMIT={}", git_commit);
     println!("cargo:rustc-env=BUILD_GIT_DATE={}", commit_date);
     println!("cargo:rustc-env=BUILD_TIME={}", build_time);
+    println!("cargo:rustc-env=BUILD_RUST_VERSION={}", rust_version);
 }
